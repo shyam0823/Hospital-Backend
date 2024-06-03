c3a40deb342a09885415c7bc4173af16915227ba
shyam0823 <shyamsundarvaka0823@gmail.com>
2024-06-03 10:55:48 +0530

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,9 @@
+package com.codegnan.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/home")
+@CrossOrigin(origins = "*")
+public class HomeController {
+}
